7b04185ef99d8e597302306e6e888e28296c0cac
Wentao Lu <wlu4@ualberta.ca>
2022-10-27 18:32:25 -0600

    count city function implemented

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -90,8 +90,8 @@ public class CustomList extends ArrayAdapter<City> {
 
     public int countCities(){
 
-//        return cities.size();
-        return 0;
+        return cities.size();
+
     }
 
 }
